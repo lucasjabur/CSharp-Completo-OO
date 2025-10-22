7b5ed0a3a89e59658be46a27e329a4992b2bf9fc
Lucas Cardoso Jabur <lucas.cardoso.jabur@outlook.com>
2025-10-22 12:08:32 -0300

    NEW: Uploading initial version of class 'Posicao'

---
+++ JogoDeXadrez/JogoDeXadrez/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tabuleiro;
+
+namespace JogoDeXadrez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao posicao = new Posicao(3, 4);
+            Console.WriteLine(posicao);
+        }
+    }
+}
