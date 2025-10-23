2fdedf0cdb257ff8a46d9660a7d1583470ceb521
Lucas Cardoso Jabur <lucas.cardoso.jabur@outlook.com>
2025-10-23 08:42:38 -0300

    UPDATE: Improving functionality of the method 'PrintBoard()'

--- JogoDeXadrez/JogoDeXadrez/Board/Screen.cs
+++ JogoDeXadrez/JogoDeXadrez/Board/Screen.cs
@@ -5,19 +5,34 @@ namespace Board {
     class Screen {
         public static void PrintBoard(GameBoard board) {
             for (int i = 0; i < board.NumberOfRows; i++) {
+                Console.Write(8 - i + " | ");
                 for (int j = 0; j < board.NumberOfColumns; j++) {
 
                     // Deve-se acessar as peças para imprimi-las no tabuleiro
                     // Porém, a matriz de peças é privada, então deve criar um método público
                     // que irá retornar uma peça em uma determinada posição do tabuleiro
                     if (board.PieceOnBoard(i, j) == null) {
-                        Console.Write("-  ");
+                        Console.Write("-   ");
                     } else {
-                        Console.Write(board.PieceOnBoard(i, j) + "  ");
+                        PrintPiece(board.PieceOnBoard(i, j));
+                        Console.Write("   ");
                     }
                 }
 
                 Console.WriteLine();
+            }
+            Console.WriteLine("   _______________________________");
+            Console.WriteLine("\n    A   B   C   D   E   F   G   H");
+        }
+
+        public static void PrintPiece(Piece piece) {
+            if (piece.Color == Color.White) {
+                Console.Write(piece);
+            } else {
+                ConsoleColor consoleColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = consoleColor;
             }
         }
     }
--- JogoDeXadrez/JogoDeXadrez/Program.cs
+++ JogoDeXadrez/JogoDeXadrez/Program.cs
@@ -8,18 +8,23 @@ namespace JogoDeXadrez
     {
         static void Main(string[] args) {
 
-            //try {
-            //    GameBoard board = new GameBoard(8, 8);
+            try {
+                GameBoard board = new GameBoard(8, 8);
 
-            //    board.PlacePiece(new Tower(Color.Black, board), new Position(0, 0));
-            //    board.PlacePiece(new Tower(Color.Black, board), new Position(1, 9));
-            //    board.PlacePiece(new King(Color.Black, board), new Position(0, 2));
-            //    Screen.PrintBoard(board);
+                board.PlacePiece(new Tower(Color.Black, board), new Position(0, 0));
+                board.PlacePiece(new Tower(Color.Black, board), new Position(1, 6));
+                board.PlacePiece(new King(Color.Black, board), new Position(0, 2));
 
-            //    Console.ReadLine();
-            //} catch (Exception ex) {
-            //    Console.WriteLine(ex.Message);
-            //}
+                board.PlacePiece(new Tower(Color.White, board), new Position(0, 4));
+                board.PlacePiece(new King(Color.White, board), new Position(2, 5));
+                board.PlacePiece(new Tower(Color.White, board), new Position(4, 6));
+
+                Screen.PrintBoard(board);
+
+                Console.ReadLine();
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
 
             ChessBoardPosition boardPosition = new ChessBoardPosition('c', 7);
             Console.WriteLine(boardPosition);
