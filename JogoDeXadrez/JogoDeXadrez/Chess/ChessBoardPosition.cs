fd05452b9af87a0708d64e47bfb6cae65af1f225
Lucas Cardoso Jabur <lucas.cardoso.jabur@outlook.com>
2025-10-23 08:29:36 -0300

    NEW: Creating 'ChessBoardPosition' class to manage the numbers and letters of a default chess board

---
+++ JogoDeXadrez/JogoDeXadrez/Chess/ChessBoardPosition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Board;
+
+namespace Chess {
+    class ChessBoardPosition {
+        public char Column { get; set; }
+        public int Row { get; set; }
+
+        public ChessBoardPosition(char column, int row) {
+            Column = char.ToUpper(column);
+            Row = row;
+        }
+
+        public Position ConvertPosition() {
+            return new Position(8 - Row, Column - 'A');
+        }
+
+        public override string ToString() {
+            return "" + Column + Row;
+        }
+    }
+}
--- JogoDeXadrez/JogoDeXadrez/Program.cs
+++ JogoDeXadrez/JogoDeXadrez/Program.cs
@@ -8,18 +8,22 @@ namespace JogoDeXadrez
     {
         static void Main(string[] args) {
 
-            try {
-                GameBoard board = new GameBoard(8, 8);
+            //try {
+            //    GameBoard board = new GameBoard(8, 8);
 
-                board.PlacePiece(new Tower(Color.Black, board), new Position(0, 0));
-                board.PlacePiece(new Tower(Color.Black, board), new Position(1, 9));
-                board.PlacePiece(new King(Color.Black, board), new Position(0, 2));
-                Screen.PrintBoard(board);
+            //    board.PlacePiece(new Tower(Color.Black, board), new Position(0, 0));
+            //    board.PlacePiece(new Tower(Color.Black, board), new Position(1, 9));
+            //    board.PlacePiece(new King(Color.Black, board), new Position(0, 2));
+            //    Screen.PrintBoard(board);
 
-                Console.ReadLine();
-            } catch (Exception ex) {
-                Console.WriteLine(ex.Message);
-            }
+            //    Console.ReadLine();
+            //} catch (Exception ex) {
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            ChessBoardPosition boardPosition = new ChessBoardPosition('c', 7);
+            Console.WriteLine(boardPosition);
+            Console.WriteLine(boardPosition.ConvertPosition());
         }
     }
 }
